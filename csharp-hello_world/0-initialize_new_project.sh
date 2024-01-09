0ed7e665ac77856681232c94d2f7af1e9d71e103
colbertamir <colbertamir1516@gmail.com>
2024-01-08 23:13:11 -0600

    Trying to push and compile

---
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create a new directory
+mkdir -p 0-new_project
+
+# Navigates into directory
+cd 0-new_project
+
+# Initializes a new C# project using dotnet
+dotnet new console
+
---
+++ csharp-hello_world/README.md
@@ -0,0 +1,3 @@
+# C# Hello World
+
+## Amir Colbert
