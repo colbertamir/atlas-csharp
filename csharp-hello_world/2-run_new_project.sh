da8a6d89cc7af5002baf3a22bfc0b5788bc6489f
colbertamir <colbertamir1516@gmail.com>
2024-01-11 11:28:07 -0600

    Change

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Initializes a new C# console project using dotnet
-dotnet new console -o "1-new_project"
+dotnet new console -o "2-new_project"
 
 # Build C# project
-dotnet build "1-new_project"
+dotnet build "2-new_project"
 
 # Runs dotnet
-cd "1-new_project" && dotnet run
+cd "2-new_project" && dotnet run
