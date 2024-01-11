b32b46d55709ff7fdf9beab8168a4023abf18af2
colbertamir <colbertamir1516@gmail.com>
2024-01-11 10:32:57 -0600

    1

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Creates new directory
-mkdir -p 1-new_project
-
-# Navigates into directory
-cd 1-new_project
-
 # Initializes a new C# console project using dotnet
-dotnet new console
+dotnet new console -o "1-new_project"
 
 # Build C# project
-dotnet build
+dotnet build "1-new_project"
