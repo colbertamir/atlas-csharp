72b65140d05fb977ac1e26ce3bdb55e018737451
colbertamir <colbertamir1516@gmail.com>
2024-01-12 02:58:37 -0600

    Divide

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ListUtils
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is null");
+            return null;
+        }
+
+        List<bool> result = new List<bool>(myList.Count);
+
+        foreach (int number in myList)
+        {
+            result.Add(number % 2 == 0);
+        }
+
+        return result;
+    }
+}
