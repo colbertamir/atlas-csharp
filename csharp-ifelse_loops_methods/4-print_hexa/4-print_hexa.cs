704ec70f41e232a4134811b1031e4afb5405a52e
colbertamir <colbertamir1516@gmail.com>
2024-01-12 00:26:22 -0600

    4-print_hexa

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 99; i++)
+        {
+            Console.Write($"{i} {i.ToString("X")}{(i < 98 ? ", " : "")}");
+        }
+    }
+}
