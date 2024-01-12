f61ab29a208c14d7f14c80781a5dadc51a3b0855
colbertamir <colbertamir1516@gmail.com>
2024-01-12 10:58:19 -0600

    Delete at

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("Input list is null");
+            return null;
+        }
+
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        List<int> updatedList = new List<int>(myList.Count - 1);
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+            {
+                updatedList.Add(myList[i]);
+            }
+        }
+
+        return updatedList;
+    }
+}
