ba55b0704381ee3a1cbc03ce082a75e3185bd170
colbertamir <colbertamir1516@gmail.com>
2024-01-12 01:14:24 -0600

    Print array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine(string.Join(", ", newArray));
+        }
+
+        return newArray;
+    }
+}
