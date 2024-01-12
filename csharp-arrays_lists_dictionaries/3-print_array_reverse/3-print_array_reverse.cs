d1816ba2d25fb95c7cb9cad6b4ce1d898bc64087
colbertamir <colbertamir1516@gmail.com>
2024-01-12 02:07:38 -0600

    Reverse

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        // Check if the array is not null
+        if (array == null)
+        {
+            Console.WriteLine("Array is null");
+            return;
+        }
+
+        // Reverse the array in-place
+        Array.Reverse(array);
+
+        // Print the reversed array
+        Console.WriteLine("Reversed Array: " + string.Join(", ", array));
+    }
+}
