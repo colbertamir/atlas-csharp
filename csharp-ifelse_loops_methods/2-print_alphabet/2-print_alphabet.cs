29362740ae3aa4c03818f1e8ff42132b32f52933
colbertamir <colbertamir1516@gmail.com>
2024-01-11 23:58:31 -0600

    alphabet

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char currentChar = 'a';
+
+        for (int i = 0; i < 26; i++)
+        {
+            Console.Write(currentChar);
+            currentChar++;
+        }
+
+        Console.WriteLine();
+    }
+}
