023e797e86f016a979194e0975e57e19b28b5912
colbertamir <colbertamir1516@gmail.com>
2024-01-12 00:37:06 -0600

    Output

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int i = 0; i < 99; i++)
         {
-            Console.WriteLine($"[{i} = 0x{i.ToString("x")}]");
+            Console.WriteLine($"{i} = 0x{i.ToString("x")}");
         }
     }
 }
