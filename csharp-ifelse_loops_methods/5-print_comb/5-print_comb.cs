2b2c8e038f4eb1847153f7f7912ddb625f048c26
colbertamir <colbertamir1516@gmail.com>
2024-01-12 00:46:02 -0600

    Comb

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{i:D2}{(i < 99 ? ", " : "\n")}");
+        }
+    }
+}
