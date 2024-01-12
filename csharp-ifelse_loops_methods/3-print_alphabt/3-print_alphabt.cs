a86e40f234d597e2b689f4e89a6d4b4f560371f7
colbertamir <colbertamir1516@gmail.com>
2024-01-12 00:16:10 -0600

    Print alphabet except-

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char currentChar = 'a';
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (currentChar != 'e' && currentChar != 'q')
+            {
+                Console.Write(currentChar);
+            }
+            currentChar++;
+        }
+    }
+}
