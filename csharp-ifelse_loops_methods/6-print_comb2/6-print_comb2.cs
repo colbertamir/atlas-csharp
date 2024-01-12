575ce51c7192b89e76c4ce737ca6662c8d6450df
colbertamir <colbertamir1516@gmail.com>
2024-01-12 00:57:04 -0600

    Comb2

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                Console.Write($"{i}{j}{(i == 8 && j == 9 ? "\n" : ", ")}");
+            }
+        }
+    }
+}
