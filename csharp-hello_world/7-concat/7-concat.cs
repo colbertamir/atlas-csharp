284e0ec0fe53130dbd79b3c45d18c62f46fa6604
colbertamir <colbertamir1516@gmail.com>
2024-01-13 10:50:13 -0600

    Concat

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        Console.WriteLine($"Welcome to {str1} {str2}!");
+    }
+}
