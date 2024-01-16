646c0ffbd42b76f1b3afb4bd6e76d7d6ba8b8ba1
colbertamir <colbertamir1516@gmail.com>
2024-01-16 01:30:13 -0600

    Print the last digit

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DictionaryUtils
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return -1;
+        }
+
+        int keyCount = 0;
+
+        foreach (var keyValuePair in myDict)
+        {
+
+            keyCount++;
+        }
+
+        return keyCount;
+    }
+}
