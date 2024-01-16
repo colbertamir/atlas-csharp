c5eec11296ad4c7932c12f9b07f73b2ded6197c7
colbertamir <colbertamir1516@gmail.com>
2024-01-16 01:19:19 -0600

    Fizz

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class FizzBuzzProgram
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            // Checks for multiples of both three and five first
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("FizzBuzz");
+            }
+            // Checks for multiples of three
+            else if (i % 3 == 0)
+            {
+                Console.Write("Fizz");
+            }
+            // Checks for multiples of five
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            // Prints number for other cases
+            else
+            {
+                Console.Write(i);
+            }
+
+            // Prints a space after each element
+            Console.Write(" ");
+        }
+
+        // Prints a new line at the end
+        Console.WriteLine();
+    }
+}
