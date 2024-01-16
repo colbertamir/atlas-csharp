5ba51c276e830b9d88d319f430c915cc409f57ac
colbertamir <colbertamir1516@gmail.com>
2024-01-16 01:09:08 -0600

    Diagonal

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            // Draws diagonal line with backslashes
+            for (int i = 0; i < length; i++)
+            {
+                // Print spaces before the backslash to create the diagonal effect
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("\\");
+            }
+        }
+
+        // Prints new line
+        Console.WriteLine();
+    }
+}
