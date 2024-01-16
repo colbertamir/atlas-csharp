50decca465d261a2533b56e043e52ae5d7a2a442
colbertamir <colbertamir1516@gmail.com>
2024-01-16 00:21:07 -0600

    Add

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int Add(int a, int b)
+    {
+        // Adds the two integers
+        int result = a + b;
+
+        // Returns result
+        return result;
+    }
+}
