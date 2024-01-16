a29dbcee066535d1c33e9da66a090f53767f5c82
colbertamir <colbertamir1516@gmail.com>
2024-01-16 00:29:46 -0600

    Print line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+        {
+            // Draws the line with underscores
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_");
+            }
+        }
+
+        // Prints new line
+        Console.WriteLine();
+    }
+}
