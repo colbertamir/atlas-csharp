0cd121e32e3899e0b0834c37a8adb55d8a7977d4
colbertamir <colbertamir1516@gmail.com>
2024-01-15 23:50:11 -0600

    Is Lower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        // Check if the character is a lowercase letter
+        return c >= 'a' && c <= 'z';
+    }
+}
