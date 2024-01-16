97b08d789c63894570493abcdff3f87081f55e2e
colbertamir <colbertamir1516@gmail.com>
2024-01-15 23:58:20 -0600

    Print the last digit

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        // Ensure number is non-negative
+        number = Math.Abs(number);
+
+        // Extract the last digit
+        int lastDigit = number % 10;
+
+        // Print the last digit
+        Console.WriteLine($"The last digit of {number} is {lastDigit}");
+
+        // Return the last digit
+        return lastDigit;
+    }
+}
