c8f2708451cf1bf579289450912f51b9e1b9e76a
colbertamir <colbertamir1516@gmail.com>
2024-01-16 20:01:00 -0600

    1-dog

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Defines Rating enum
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+// Defines Dog struct
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
