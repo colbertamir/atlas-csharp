16c4127515e1333579ae681cacc38c7b2a5301f3
colbertamir <colbertamir1516@gmail.com>
2024-01-17 01:13:20 -0600

    3-type_get

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>
+/// Supplies utility methods for working with objects.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Prints the names of available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">The object to inspect.</param>
+    public static void Print(object myObj)
+    {
+        if (myObj == null)
+        {
+            Console.WriteLine("Object is null.");
+            return;
+        }
+
+        Type objType = myObj.GetType();
+        PropertyInfo[] properties = objType.GetProperties();
+        MethodInfo[] methods = objType.GetMethods();
+
+        Console.WriteLine("Properties:");
+        foreach (var property in properties)
+        {
+            Console.WriteLine($"  {property.Name}");
+        }
+
+        Console.WriteLine("\nMethods:");
+        foreach (var method in methods)
+        {
+            Console.WriteLine($"  {method.Name}");
+        }
+    }
+}
