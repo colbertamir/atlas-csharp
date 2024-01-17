f22340529688bf8134ff208c6e866cfddc4b9cc5
colbertamir <colbertamir1516@gmail.com>
2024-01-17 12:36:23 -0600

    4-inherit

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Represents base class for animals.
+/// </summary>
+public class Animal
+{
+    // Any methods for the Animal class can be added here
+}
+
+/// <summary>
+/// Represents a dog.
+/// </summary>
+public class Dog : Animal
+{
+    // Any methods specific to the Dog class can be added here
+}
