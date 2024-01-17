b1b945f4df97b6d464b94a86a6975d80e1863a02
colbertamir <colbertamir1516@gmail.com>
2024-01-16 23:38:26 -0600

    3-enemy

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents health of the zombie.
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with default health (0).
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with a specified health value.
+        /// </summary>
+        /// <param name="value">The initial health value. Must be greater than or equal to 0.</param>
+        /// <exception cref="ArgumentException">Thrown when the specified health value is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            health = value;
+        }
+
+        /// <summary>
+        /// Gets the current health of Zombie.
+        /// </summary>
+        /// <returns>The current health of Zombie.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
