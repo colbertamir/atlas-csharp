f1376bdc2fff3f68edb5966f2e0ed241c0653af8
colbertamir <colbertamir1516@gmail.com>
2024-01-16 19:34:55 -0600

    0-dog

---
+++ csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Defines Rating enum
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rating rating;
+
+        rating = Rating.Great;
+
+        Console.WriteLine("Score: " + rating);
+        Console.WriteLine("Score: " + (int)rating);
+    }
+}
