cc69278141110ceaf3017e6cf8b368d3413c8d83
colbertamir <colbertamir1516@gmail.com>
2024-01-17 01:02:09 -0600

    2-subclass

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Supplies utility methods for working with objects.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Checks if the object is an instance of a class that inherits from the specified class.
+    /// </summary>
+    /// <param name="derivedType">The type of derived class.</param>
+    /// <param name="baseType">The type of the base class.</param>
+    /// <returns>True if the object is a subclass specified base class; otherwise, False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return baseType.IsAssignableFrom(derivedType) && derivedType != baseType;
+    }
+}
