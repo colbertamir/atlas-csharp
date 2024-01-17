3b41328a19b8c8ce815817e06a39045d38651ced
colbertamir <colbertamir1516@gmail.com>
2024-01-16 20:11:34 -0600

    2-dog

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Defines Rating enum
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+// Defines Dog struct with constructor
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    // A constructor for Dog struct
+    public Dog(string dogName, float dogAge, string dogOwner, Rating dogRating)
+    {
+        name = dogName;
+        age = dogAge;
+        owner = dogOwner;
+        rating = dogRating;
+    }
+}
