2feb6acf269fc4a89cb72467c24b9bdf354c6c70
colbertamir <colbertamir1516@gmail.com>
2024-01-16 23:02:52 -0600

    1-enemy

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes new instance of <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
