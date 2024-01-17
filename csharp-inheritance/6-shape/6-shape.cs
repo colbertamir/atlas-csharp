43a5e0b83445579479f5e9ea6c85a7bcf899ea85
colbertamir <colbertamir1516@gmail.com>
2024-01-17 12:50:59 -0600

    6-shape

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+/// <summary>
+/// Represents a shape.
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Calculates and returns area of the shape.
+    /// </summary>
+    /// <returns>Area of the shape.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Represents a rectangle.
+/// </summary>
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Gets or sets width of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when the width is negative.</exception>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            }
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets height of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when provided height is negative.</exception>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            }
+            height = value;
+        }
+    }
+
+    /// <summary>
+    /// Calculates and returns the area of rectangle.
+    /// </summary>
+    /// <returns>Area of the rectangle.</returns>
+    public override int Area()
+    {
+        return width * height;
+    }
+}
