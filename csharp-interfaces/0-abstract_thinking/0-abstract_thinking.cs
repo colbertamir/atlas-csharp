8a79c679f6b97b24578dd09b3f0ec0c85ef904ea
colbertamir <colbertamir1516@gmail.com>
2024-01-17 15:47:17 -0600

    0-abstract_thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Represents a abstract base class.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the object.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Returns a string representation of object.
+    /// </summary>
+    /// <returns>Formatted string representing the object.</returns>
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}";
+    }
+}
