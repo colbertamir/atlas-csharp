f4cc49ac866e0aa07d7c499bb57863c3498695c4
colbertamir <colbertamir1516@gmail.com>
2024-01-17 21:01:41 -0600

    HMMM

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -8,14 +8,14 @@ public abstract class Base
     /// <summary>
     /// Gets or sets the name of the object.
     /// </summary>
-    public string Name { get; set; }
+    public string name;
 
     /// <summary>
     /// Returns a string representation of the object.
     /// </summary>
     /// <returns>A formatted string representing the object.</returns>
-    public override string ToString()
+public override string ToString()
     {
-        return $"{Name} is a {this.GetType()}";
+        return string.Format("{0} is a {1}", name, this.GetType().ToString());
     }
 }
