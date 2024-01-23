f96b7d66b1c243ff698552b6d3f11423c3ba91f3
colbertamir <colbertamir1516@gmail.com>
2024-01-23 15:36:04 -0600

    count

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -5,40 +5,45 @@
 public class Queue<T>
 {
     /// <summary>
-    /// Represents a node in queue.
+    /// Represents a node in the queue.
     /// </summary>
     public class Node
     {
         /// <summary>
-        /// Gets or sets value of the node.
+        /// Gets or sets the value of the node.
         /// </summary>
-        public T Value { get; set; } = default(T);
+        public T value { get; set; } = default(T);
 
         /// <summary>
-        /// Gets or sets next node in the queue.
+        /// Gets or sets the next node in the queue.
         /// </summary>
-        public Node Next { get; set; } = null;
+        public Node next { get; set; } = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Node"/> class.
         /// </summary>
-        /// <param name="value">The initial value of node.</param>
+        /// <param name="value">The initial value of the node.</param>
         public Node(T value)
         {
-            Value = value;
-            Next = null;
+            value = value;
+            next = null;
         }
     }
 
     /// <summary>
-    /// Gets or sets head node of the queue.
+    /// Gets or sets the head node of the queue.
     /// </summary>
-    public Node Head { get; set; } = null;
+    public Node head { get; set; } = null;
 
     /// <summary>
-    /// Gets or sets tail node of the queue.
+    /// Gets or sets the tail node of the queue.
     /// </summary>
-    public Node Tail { get; set; } = null;
+    public Node tail { get; set; } = null;
+
+    /// <summary>
+    /// Gets or sets the count of elements in the queue.
+    /// </summary>
+    public int count { get; private set; } = 0;
 
     /// <summary>
     /// Checks the type of the queue.
@@ -50,32 +55,32 @@ public class Queue<T>
     }
 
     /// <summary>
-    /// Adds new node to the end of the queue.
+    /// Adds a new node to the end of the queue.
     /// </summary>
     /// <param name="value">The value to be added to the queue.</param>
     public void Enqueue(T value)
     {
         Node newNode = new Node(value);
 
-        if (Head == null)
+        if (head == null)
         {
             // If the queue is empty, the new node becomes both the head and tail
-            Head = newNode;
-            Tail = newNode;
+            head = newNode;
+            tail = newNode;
         }
         else
         {
             // Otherwise, add the new node to the end of the queue
-            Tail.Next = newNode;
-            Tail = newNode;
+            tail.next = newNode;
+            tail = newNode;
         }
 
         // Increment the count
-        Count++;
+        count++;
     }
 
     /// <summary>
-    /// Returns number of nodes in the queue.
+    /// Returns the number of nodes in the queue.
     /// </summary>
     /// <returns>The count of nodes in the queue.</returns>
     public int Count()
