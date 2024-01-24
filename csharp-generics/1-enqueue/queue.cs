f550e55e8b976979327b6421f55918d8a419d070
colbertamir <colbertamir1516@gmail.com>
2024-01-24 16:29:32 -0600

    Compilation error

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -40,9 +40,9 @@
     /// Returns the number of nodes in the queue.
     /// </summary>
     /// <returns>The number of nodes in the queue.</returns>
-    public int Count()
+    public int Count
     {
-        return count;
+        get { return count; }
     }
 
     private int count = 0;
